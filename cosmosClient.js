3dfe9eee96c63f8b4ff52cfb678bf5aefa63b6e1
LeapgenSolution <websites@leapgen.ai>
2025-05-22 18:51:52 -0500

    Updated git ignore

--- cosmosClient.js
+++ cosmosClient.js
@@ -2,8 +2,7 @@ const { CosmosClient } = require("@azure/cosmos");
 require("dotenv").config();
 
 const endpoint = process.env.COSMOS_ENDPOINT;
-// const key = atob(process.env.COSMOS_KEY);
-const key = atob("V05qRVJaOXRpUzZhbFBkbjZJVFB3T0gzYWgxMjBQaDM2MW5IckRrQXBwd0xyQXQ1T2U0dlh4UHRFbGd1Y2lGcG9QaWxsd21qZjFta0FDRGJuYjFYc1E9PQ");
+const key = process.env.COSMOS_KEY;
 
 const databaseId = process.env.COSMOS_DATABASE;
 const containerId = process.env.COSMOS_CONTAINER;

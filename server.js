5edd49223e980e104bbb2a22cd51e81c4c8db30c
LeapgenSolution <websites@leapgen.ai>
2025-06-05 23:35:38 -0500

    WEbhook added

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const cors = require("cors");
 const { fetchAllAppointments, fetchAllPatients,
   fetchSOAPByAppointment, fetchBillingByAppointment,
   fetchSummaryByAppointment, fetchTranscriptByAppointment,
-  fetchReccomendationByAppointment, 
-  patchBillingByAppointment} = require("./cosmosClient");
-const { StreamClient } = require("@stream-io/node-sdk");
+  fetchReccomendationByAppointment,
+  patchBillingByAppointment } = require("./cosmosClient");
+const { StreamClient, StreamVideoClient } = require("@stream-io/node-sdk");
 const { storageContainerClient, upload } = require("./blobClient");
 const { sendMessage } = require("./serviceBusClient");
+const { default: axios } = require("axios");
 
 config();
 
@@ -87,7 +88,7 @@ app.get("/api/billing/:id", async (req, res) => {
 
 app.patch("/api/billing/:id", async (req, res) => {
   try {
-    const { id } = req.params    
+    const { id } = req.params
     await patchBillingByAppointment(id, req.query.username, req.body.billing_codes)
     res.status(200).json({ success: true })
   } catch (error) {
@@ -146,7 +147,7 @@ app.get("/api/recommendations/:id", async (req, res) => {
   }
 });
 
-app.post("/get-token", (req, res) => {
+app.post("/get-token", async (req, res) => {
   console.log(req.body);
 
   const { userId } = req.body;
@@ -199,6 +200,54 @@ app.post("/api/end-call/:appointmentId", async (req, res) => {
     res.status(500).json({ error: "Failed to send message to queue" })
   }
 
+})
+
+app.post("/webhook", async (req, res) => {
+  const { type } = req.body;
+  if (type === 'call.recording_ready') {
+    console.log(req.body);
+
+    const { url: videoUrl, call_cid, filename } = req.body.call_recording;
+    const response = await axios.get(videoUrl, { responseType: 'arraybuffer' });
+    // const mp4Buffer = await convertMp4FromUrl(response.data);
+    const buffer = Buffer.from(response.data);
+    // const existing = readData();
+    const client = new StreamClient(process.env.STREAM_IO_APIKEY, process.env.STREAM_IO_SECRET);
+    const call = await client.video.getCall({
+      id: call_cid.split(":")[1],
+      type: "default"
+    })
+    const username = call.call.created_by.name
+    const meetingChunks = filename.split("_")
+    const meetingChunkName = meetingChunks[meetingChunks.length - 1]
+    const blobName = `${username}/${call_cid.split(":")[1]}/meeting_part${meetingChunkName}`;
+    console.log(call.call.created_by.name);
+    const blobClient = storageContainerClient.getBlockBlobClient(blobName);
+    await blobClient.uploadData(buffer, {
+      blobHTTPHeaders: {
+        blobContentType: 'video/mp4'
+      }
+    });
+
+    // const blobClient = storageContainerClient.getBlockBlobClient(blobName);
+    // writeData([...existing, newRecord]);
+    //     {
+    //    type: 'call.recording_ready',
+    //    created_at: '2025-06-06T21:23:00.427846676Z',
+    //    call_cid: 'default:2025-06-06_1015',
+    //    call_recording: {
+    //      filename: 'rec_default_2025-06-06_1015_720p_1749244938038.mp4',
+    //      url: 'https://us-east.stream-io-cdn.com/1388924/video/recordings/default_2025-06-06_1015/rec_default_2025-06-06_1015_720p_1749244938038.mp4?Expires=1750454580&Signature=e2JtlLOq0NhClHKvY8QeVoHEhF3yrCQjQnlkp9jrEVN-mK81xcUbupNkS8XFYeVgpOheJRiOvObO4uUcrwkDTeOph3HOI334lR1KDoEvuxIBfvNXI0-bz4CHy1DH7FPeKqpvVvIsjMjyNeiKKm5yfCk1y~NSFW229MUWaBJqUy~Vqp45LGFno2f9~SBIKJOshrk6wkzrZ1OfCwJSXSWDmmu8ERe~j5zVnTw6KRet3wFMyB7ajDWEs2Xr5OEa7ETf-V3OMFSesA9ek5KGzh0t76sF8mg9UErRg0RnrbRhOm6Vkyl-OznSd7fjvOCNj3bwK6b2fQ1GB1mzOTyqfAjCdw__&Key-Pair-Id=APKAIHG36VEWPDULE23Q',
+    //      start_time: '2025-06-06T21:22:22.891526071Z',
+    //      end_time: '2025-06-06T21:22:53.691208901Z',
+    //      session_id: '6680d971-2036-4627-9baa-84684a536f34'
+    //    }
+
+    console.log(`✅ Saved recording for ${call_cid}`);
+    return res.status(200).json(newRecord);
+  }
+
+  // res.sendStatus(204); // ignored
 })
 
 httpServer.listen(PORT, () =>

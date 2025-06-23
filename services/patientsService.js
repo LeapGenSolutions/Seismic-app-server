a1fe267609a79b86651026499c2ad0a0144fb02c
LeapgenSolution <websites@leapgen.ai>
2025-06-23 09:18:54 -0500

    Changes to Patients API

--- services/patientsService.js
+++ services/patientsService.js
@@ -1,17 +1,65 @@
 const { CosmosClient } = require("@azure/cosmos");
+const { param } = require("../routes/callHistory");
 require("dotenv").config();
 
 const endpoint = process.env.COSMOS_ENDPOINT;
 const key = process.env.COSMOS_KEY;
-const databaseId = process.env.COSMOS_DATABASE;
+const databaseId = "seismic-chat-bot";
 const client = new CosmosClient({ endpoint, key });
 
 async function fetchAllPatients() {
     const database = client.database(databaseId);
-    const container = database.container("patients");
-    const querySpec = { query: "SELECT * from c" };
+    const container = database.container("Patients");
+    const querySpec = { query: "SELECT c.original_json from c" };
     const { resources: items } = await container.items.query(querySpec).fetchAll();
-    return items;
+    const result = items.map(item => {
+
+        if (item?.original_json?.details) {
+            return {
+                "patient_id": item?.original_json?.patient_id,
+                "practice_id": item?.original_json?.practice_id,
+                ...item?.original_json?.details
+            }
+        }
+        if (!item?.original_json?.details) {
+            return {
+                "patient_id": item?.original_json?.patientID,
+                "practice_id": item?.original_json?.practiceID,
+                ...item?.original_json?.original_json?.details
+            }
+        }
+    })
+    return result;
 }
 
-module.exports = { fetchAllPatients };
+async function fetchPatientById(patient_id) {
+    const database = client.database(databaseId);
+    const container = database.container("Patients");
+    const querySpec = {
+        query: "SELECT c.original_json from c where c.patientID = @patientId",
+        parameters: [{ name: "@patientId", value: Number(patient_id) }]
+    };
+    const { resources: items } = await container.items.query(querySpec).fetchAll();
+    const item = items[0];
+    let result = {};
+    if (item?.original_json?.details) {
+        result =  {
+            "patient_id": item?.original_json?.patient_id,
+            "practice_id": item?.original_json?.practice_id,
+            ...item?.original_json?.details
+        }
+    }
+    if (!item?.original_json?.details) {
+        result = {
+            "patient_id": item?.original_json?.patientID,
+            "practice_id": item?.original_json?.practiceID,
+            ...item?.original_json?.original_json?.details
+        }
+    }
+    return result;
+}
+
+module.exports = {
+    fetchAllPatients,
+    fetchPatientById
+};

c1d07c17f4ffef83fef674f464910f99cccab043
LeapgenSolution <websites@leapgen.ai>
2025-06-23 11:53:42 -0500

    Changes to Appointment API

--- services/appointmentsService.js
+++ services/appointmentsService.js
@@ -9,13 +9,33 @@ const client = new CosmosClient({ endpoint, key });
 async function fetchAppointmentsByEmail(email) {
     const database = client.database(databaseId);
     const container = database.container("seismic_appointments");
-    const querySpec = { query: `SELECT * from c` };
+    const querySpec = {
+        query: `SELECT 
+                    c.id AS appointment_date,
+                    d.id,
+                    d.type,
+                    d.first_name,
+                    d.last_name,
+                    d.full_name,
+                    d.ssn,
+                    lower(d.doctor_name),
+                    d.doctor_id,
+                    d.doctor_email,
+                    d.specialization,
+                    d.time,
+                    d.status,
+                    d.insurance_provider,
+                    d.email,
+                    d.phone,
+                    d.insurance_verified,
+                    d.patient_id,
+                    d.practice_id
+                FROM c
+                JOIN d IN c.data where lower(d.doctor_email) = @doctorEmail`,
+        parameters: [{ name: "@doctorEmail", value: email.toLowerCase() }]
+    };
     const { resources: items } = await container.items.query(querySpec).fetchAll();
-    const filtered_items = items.map(item => ({
-        id: item.id,
-        data: item.data.filter((item_t) => item_t.doctor_email.toLowerCase().includes(email.toLowerCase()))
-    }));
-    return filtered_items;
+    return items;
 }
 
 module.exports = { fetchAppointmentsByEmail };

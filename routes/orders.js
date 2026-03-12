b10067c38b417c570d87a4cf83e53f39ffd9490c
Vamshi Krishna <vamshikrishnadulam2504@gmail.com>
2026-03-12 18:46:19 +0530

    Edit-Orders-Api

--- routes/orders.js
+++ routes/orders.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const { postOrdersReferral, postOrdersVaccine, postOrdersProcedure, postOrdersPrescription, postOrdersPatientInfo, postOrdersOther, postOrdersLab, postOrdersImaging, postOrdersDME, postOrdersAll} = require("../services/ordersService");
+const { patchSoapNotesByAppointment } = require("../services/soapService");
 const router = express.Router();
 
+router.patch("/:email/:id/update", async (req, res) => {
+    const { id, email } = req.params;
+    const { orders } = req.body;
+    if (!orders || !Array.isArray(orders)) {
+        return res.status(400).json({ message: "Orders must be provided as an array" });
+    }
+    try {
+        updatedOrders = {
+            orders: orders
+        }
+        const soapId = `${email}_${id}_soap`;
+        await patchSoapNotesByAppointment(soapId, email, updatedOrders);
+        return res.status(200).json({ message: "Orders updated successfully" });
+    } catch (error) {
+        console.error("Error updating SOAP notes:", error);
+        return res.status(500).json({ message: "Failed to update SOAP notes" });
+    }
+});
 // post orders
 
 router.post("/:email/encounters/:encounterId/orders/imaging", async (req, res) => {
